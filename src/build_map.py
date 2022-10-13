a1bcc539730255ba5d9db29340e331da6062637b
TheLonelyFighter <mihaiandrei1987@yahoo.com>
2022-10-13 13:34:24 +0300

    updated dependencies

--- src/build_map.py
+++ src/build_map.py
@@ -1,5 +1,3 @@
-# importing the requests library
-from numpy import size
 import requests
 import shutil
 
@@ -8,11 +6,11 @@ flight_zone = None #define as a pair of coordinates determining a rectangle in w
 patch_size = None #define as height (latitude) and width (longitude) of the patch to be taken
 
 center = "60.403066,22.462214"
-zoom = "18"
-size = "640x640"
+zoom = "18" # optimized for the perspective of a wide angle camera at 120 m altitude
+size = "640x640"  # maximum allowed size
 maptype = "satellite"
-scale = "2"
-key = "AIzaSyAclBCbWo0rwQIaezGcXM6X3S_Otv-hHOQ"
+scale = "2" # maximum allowed scale
+key = "AIzaSyAclBCbWo0rwQIaezGcXM6X3S_Otv-hHOQ" #restricted by IP address, so it will not work if you are not in the lab
 
 #URL = "https://maps.googleapis.com/maps/api/staticmap?center=60.403066,22.462214&zoom=18&size=640x640&scale=2&maptype=satellite&key=AIzaSyD3uC7jYBv2UDe6NO5bx8iWJ2cHIjdPSO0"
 
@@ -31,10 +29,11 @@ PARAMS = {'center':center,
           
   
 # sending get request and saving the response as response object
-#r = requests.get(url = URL, params = PARAMS)
+
 r = requests.get(url = URL, params = PARAMS, stream=True)
 
 
+# if request successful, write image to file
 if r.status_code == 200:
     with open('img.png', 'wb') as out_file:
         shutil.copyfileobj(r.raw, out_file)
