65fa9a3f84b05576e2fafa6d127b3bb2da00fa5d
TheLonelyFighter <mihaiandrei1987@yahoo.com>
2022-10-14 09:52:40 +0300

    defined classes for building map

--- src/build_map.py
+++ src/build_map.py
@@ -1,9 +1,32 @@
 import requests
 import shutil
 
-# These 2 varibles determine the number of images that form the map
-flight_zone = None #define as a pair of coordinates determining a rectangle in which the satellite photos will be taken
-patch_size = None #define as height (latitude) and width (longitude) of the patch to be taken
+class FlightZone:
+    """A rectangle shaped flight area defined by 2 points (latitudes and longitudes)"""
+    def __init__(self,top_left_lat, top_left_lon, bottom_right_lat, bottom_right_lon):
+        self.top_left_lat = top_left_lat
+        self.top_left_lon = top_left_lon
+        self.bottom_right_lat = bottom_right_lat
+        self.bottom_right_lon = bottom_right_lon
+
+    def __str__(self):
+        return "%s; \ntop_left_lat: %f \ntop_left_lon: %f \nbottom_right_lat: %f \nbottom_right_lon: %f" % (self.__class__.__name__, self.top_left_lat, self.top_left_lon, self.bottom_right_lat, self.bottom_right_lon)
+
+class PatchSize:
+    """Size of a map patch in decimal latitude and longitude"""
+    def __init__(self, lat, lon):
+        self.lat = lat
+        self.lon = lon
+
+    def __str__(self):
+        return "%s; \nlat: %f \nlon: %f" % (self.__class__.__name__, self.lat, self.lon)
+
+# These 2 variables determine the number of images that form the map
+flight_zone = FlightZone(60.408615, 22.460445, 60.400855, 22.471289) #define as a pair of coordinates determining a rectangle in which the satellite photos will be taken
+patch_size = PatchSize(0.001676, 0.00341) #define as height (latitude) and width (longitude) of the patch to be taken
+
+
+
 
 center = "60.403066,22.462214"
 zoom = "18" # optimized for the perspective of a wide angle camera at 120 m altitude
